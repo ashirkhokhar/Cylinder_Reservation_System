86d923ca6f01fab02ab0b724ddf02e8d06a3da46
Your Name <you@example.com>
2023-08-10 02:16:17 +0500

    remove screens

--- lib/Details/OrderDetails.dart
+++ lib/Details/OrderDetails.dart
@@ -6,7 +6,7 @@ import 'package:provider/provider.dart';
 
 import 'order_model.dart';
 
-class DetailsPage extends StatelessWidget {
+class DetailsPage extends StatefulWidget {
   final String selectedLocation;
   final String selectedCylinder;
   final String selectedQuantity; // New variable for cylinder size
@@ -26,13 +26,20 @@ class DetailsPage extends StatelessWidget {
   });
 
   @override
+  _DetailsPageState createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  bool _isLoading = false;
+
+  @override
   Widget build(BuildContext context) {
     final orderProvider = Provider.of<OrderProvider>(context, listen: false);
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.deepPurple[300],
-        title: Text("Selected Credenstials",
+        title: Text("Selected Credentials",
             style: GoogleFonts.bebasNeue(fontSize: 30, color: Colors.white)),
         automaticallyImplyLeading: false,
         centerTitle: true,
@@ -41,86 +48,112 @@ class DetailsPage extends StatelessWidget {
           bottom: Radius.circular(30),
         )),
       ),
-      body: Center(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-          children: [
-            ListTile(
-              title: const Text('Location'),
-              subtitle: Text(selectedLocation),
-            ),
-            ListTile(
-              title: const Text('Cylinder'),
-              subtitle: Text(selectedCylinder),
-            ),
-            ListTile(
-              title: const Text('Quantity'),
-              subtitle: Text(selectedQuantity),
-            ),
-            ListTile(
-              title: const Text('Sector'),
-              subtitle: Text(sector),
-            ),
-            ListTile(
-              title: const Text('Street'),
-              subtitle: Text(street),
-            ),
-            ListTile(
-              title: const Text('House No'),
-              subtitle: Text(houseNo),
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: GestureDetector(
-                onTap: () {
-                  orderProvider.addOrder(OrderData(
-                    selectedLocation: selectedLocation,
-                    selectedCylinder: selectedCylinder,
-                    selectedQuantity:
-                        selectedQuantity, // Make sure you have this field
-                    sector: sector, // Make sure you have this field
-                    houseNo: houseNo, // Make sure you have this field
-                    street: street, // Make sure you have this field
-                    phoneNumber: phoneNumber,
-                  ));
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Ordertable(
-                        selectedLocation: selectedLocation,
-                        selectedCylinder: selectedCylinder,
-                        selectedQuantity: selectedQuantity,
-                        phoneNumber: phoneNumber,
-                        sector: sector,
-                        houseno: houseNo,
-                        street: street,
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Center(
+            child: ListView(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              children: [
+                ListTile(
+                  title: const Text('Location'),
+                  subtitle: Text(widget.selectedLocation),
+                ),
+                ListTile(
+                  title: const Text('Cylinder'),
+                  subtitle: Text(widget.selectedCylinder),
+                ),
+                ListTile(
+                  title: const Text('Quantity'),
+                  subtitle: Text(widget.selectedQuantity),
+                ),
+                ListTile(
+                  title: const Text('Sector'),
+                  subtitle: Text(widget.sector),
+                ),
+                ListTile(
+                  title: const Text('Street'),
+                  subtitle: Text(widget.street),
+                ),
+                ListTile(
+                  title: const Text('House No'),
+                  subtitle: Text(widget.houseNo),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: GestureDetector(
+                    onTap: () async {
+                      setState(() {
+                        _isLoading = true;
+                      });
+
+                      orderProvider.addOrder(OrderData(
+                        selectedLocation: widget.selectedLocation,
+                        selectedCylinder: widget.selectedCylinder,
+                        selectedQuantity: widget.selectedQuantity,
+                        sector: widget.sector,
+                        houseNo: widget.houseNo,
+                        street: widget.street,
+                        phoneNumber: widget.phoneNumber,
+                      ));
+
+                      await Future.delayed(
+                          const Duration(seconds: 2)); // Simulate some process
+
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Ordertable(
+                            selectedLocation: widget.selectedLocation,
+                            selectedCylinder: widget.selectedCylinder,
+                            selectedQuantity: widget.selectedQuantity,
+                            phoneNumber: widget.phoneNumber,
+                            sector: widget.sector,
+                            houseno: widget.houseNo,
+                            street: widget.street,
+                          ),
+                        ),
+                      );
+
+                      setState(() {
+                        _isLoading = false;
+                      });
+                    },
+                    child: Container(
+                      width: 300,
+                      padding: const EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple[300],
+                        borderRadius: BorderRadius.circular(12),
                       ),
-                    ),
-                  );
-                },
-                child: Container(
-                  width: 300,
-                  padding: const EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurple[300],
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Ok',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
+                      child: const Center(
+                        child: Text(
+                          'Ok',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
+              ],
+            ),
+          ),
+          if (_isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.6),
+              child: const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.deepPurple,
+                ),
               ),
             ),
-          ],
-        ),
+        ],
       ),
     );
   }
--- lib/Details/Order_History.dart
+++ lib/Details/Order_History.dart
@@ -13,37 +13,51 @@ class OrdersRecords extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurple[300],
-        title: Text(
-          "Order History",
-          style: GoogleFonts.bebasNeue(fontSize: 30, color: Colors.white),
-        ),
-        // ... (other app bar settings)
+        title: Text("Order History",
+            style: GoogleFonts.bebasNeue(fontSize: 30, color: Colors.white)),
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(30),
+        )),
       ),
       body: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: DataTable(
-          columns: [
-            const DataColumn(label: Text('Location')),
-            const DataColumn(label: Text('Cylinder')),
-            const DataColumn(label: Text('Quantity')),
-            const DataColumn(label: Text('Sector')),
-            const DataColumn(label: Text('House No')),
-            const DataColumn(label: Text('Street')),
-            const DataColumn(label: Text('Phone No')),
-          ],
-          rows: orderProvider.orders.map((order) {
-            return DataRow(
-              cells: [
-                DataCell(Text(order.selectedLocation)),
-                DataCell(Text(order.selectedCylinder)),
-                DataCell(Text(order.selectedQuantity)),
-                DataCell(Text(order.sector)),
-                DataCell(Text(order.houseNo)),
-                DataCell(Text(order.street)),
-                DataCell(Text(order.phoneNumber)),
-              ],
-            );
-          }).toList(),
+        scrollDirection: Axis.vertical, // Set vertical scroll direction
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: DataTable(
+            columns: [
+              const DataColumn(
+                  label: Text('Serial No')), // Serial number column
+              const DataColumn(label: Text('Location')),
+              const DataColumn(label: Text('Cylinder')),
+              const DataColumn(label: Text('Quantity')), // Status column
+              const DataColumn(label: Text('Sector')),
+              const DataColumn(label: Text('House No')),
+              const DataColumn(label: Text('Street')),
+              const DataColumn(label: Text('Phone No')),
+              const DataColumn(label: Text('Status')),
+            ],
+            rows: orderProvider.orders.asMap().entries.map((entry) {
+              final index = entry.key + 1; // Serial number
+              final order = entry.value;
+              return DataRow(
+                cells: [
+                  DataCell(Text('$index')), // Display serial number
+                  DataCell(Text(order.selectedLocation)),
+                  DataCell(Text(order.selectedCylinder)),
+                  DataCell(Text(order
+                      .selectedQuantity)), // Display "Pending" for all orders
+                  DataCell(Text(order.sector)),
+                  DataCell(Text(order.houseNo)),
+                  DataCell(Text(order.street)),
+                  DataCell(Text(order.phoneNumber)),
+                  DataCell(Text('Pending')),
+                ],
+              );
+            }).toList(),
+          ),
         ),
       ),
     );
